1910df9ed5987f399dcb726c62b8dfee01c1cc71
Juanfran Bustos <juanfranbustosc@gmail.com>
2026-02-12 22:08:24 +0100

    Update checkCharger.js

--- checkCharger.js
+++ checkCharger.js
@@ -5,6 +5,8 @@ const CHARGER_ID = process.env.CHARGER_ID;
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
+const STATE_FILE = "state.json";
+
 async function sendTelegram(message) {
     const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
     await fetch(url, {
@@ -17,15 +19,46 @@ async function sendTelegram(message) {
     });
 }
 
+
+function getPreviousState() {
+  if (!fs.existsSync(STATE_FILE)) {
+    return null; // primera ejecución
+  }
+
+  const raw = fs.readFileSync(STATE_FILE);
+  const data = JSON.parse(raw);
+  return data.available;
+}
+
+function saveState(available) {
+  fs.writeFileSync(
+    STATE_FILE,
+    JSON.stringify({ available }, null, 2)
+  );
+}
+
 async function checkCharger() {
     const url = `https://api.tomtom.com/search/2/chargingAvailability.json?key=${TOMTOM_API_KEY}&chargingAvailability=${CHARGER_ID}`;
 
     const res = await fetch(url);
     const data = await res.json();
 
+    
+
     const available = data.connectors[0].availability.current.available ?? 0;
     const occupied = data.connectors[0].availability.current.occupied ?? 0;
     const reserved = data.connectors[0].availability.current.reserved ?? 0;
+
+      const previous = getPreviousState();
+    
+      console.log("Previous:", previous);
+      console.log("Current:", available);
+    
+      // Primera ejecución → solo guardar estado, no notificar
+      if (previous === null) {
+        saveState(available);
+        return;
+      }
 
     console.log("Available connectors:", available);
 
@@ -38,6 +71,7 @@ async function checkCharger() {
             `⚡🔴 CARGADOR OCUPADO\nHay ${occupied} conector(es) ocupado\n Hay ${reserved} cargadores reservados`
         );
     }*/
+    saveState(available);
 }
 
 checkCharger().catch(console.error);
